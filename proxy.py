24bcac368c95b6cfbd2d3c7051cadbd0009a5eed
heatherdolland-spec <heather.dolland@gmail.com>
2026-04-06 21:09:13 -0400

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,11 +1,16 @@
-from flask import Flask, request, jsonify
+ from flask import Flask, request, jsonify
 import anthropic
 import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['OPTIONS', 'POST'])
+@app.route('/', methods=['GET', 'HEAD', 'OPTIONS', 'POST'])
 def proxy():
+    if request.method in ['GET', 'HEAD']:
+        response = jsonify({'status': 'Authority Proxy Running'})
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+
     if request.method == 'OPTIONS':
         response = jsonify({})
         response.headers['Access-Control-Allow-Origin'] = '*'
@@ -15,14 +20,14 @@ def proxy():
 
     client = anthropic.Anthropic(api_key=os.environ.get('ANTHROPIC_API_KEY'))
     data = request.json
-    
+
     message = client.messages.create(
         model=data['model'],
         max_tokens=data['max_tokens'],
         system=data['system'],
         messages=data['messages']
     )
-    
+
     response = jsonify({
         'content': [{'text': message.content[0].text, 'type': 'text'}]
     })
@@ -30,5 +35,5 @@ def proxy():
     return response
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)   
